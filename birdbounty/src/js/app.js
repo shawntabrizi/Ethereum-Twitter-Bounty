ff1683f91d38a6baa6fb0294f813358d009c7cd1
Shawn Tabrizi <shawntabrizi@gmail.com>
2018-08-16 01:15:56 -0700

    main ux working end to end

--- birdbounty/src/js/app.js
+++ birdbounty/src/js/app.js
@@ -35,7 +35,7 @@ App = {
       App.contracts.BirdBounty.setProvider(App.web3Provider);
 
       // Use our contract to retrieve and mark the adopted pets
-      return App.getNumOfBounties();
+      return App.showBounties();
     });
 
     return App.bindEvents();
@@ -48,6 +48,8 @@ App = {
     $("#submit-bird-bounty").click(App.submitBirdBounty);
     $("#create-bounty-jumbo").click(App.showCreateBountyInput);
     $("#fulfill-bounty-jumbo").click(App.showFulfillBountyInput);
+    $("#fulfill-bountyid-input").change(App.checkBountyFulfill);
+    $("#fulfill-bird-bounty").click(App.claimBounty);
 
   },
 
@@ -66,14 +68,13 @@ App = {
   getNumOfBounties: function () {
     var birdBountyInstance;
 
-    App.contracts.BirdBounty.deployed().then(function (instance) {
+    return App.contracts.BirdBounty.deployed().then(function (instance) {
       birdBountyInstance = instance;
-      birdBountyInstance.getNumBounties()
-        .then(function (result) {
-          console.log("Num of bounties: " + result);
-          App.numOfBounties = result.toNumber();
-          return App.showBounties();
-        });
+      return birdBountyInstance.getNumBounties()
+    }).then(function (result) {
+      console.log("Num of bounties: " + result);
+      App.numOfBounties = result.toNumber();
+      return App.numOfBounties;
     });
   },
 
@@ -89,7 +90,7 @@ App = {
 
   showBounty: function (bountyObject, index) {
     var bountyRow = $('#bountyRow');
-    var bountyTemplate = $('#bountyTemplate');
+    var bountyTemplate = $('#bountyTemplate').clone();
 
     try {
       bountyTemplate.find('.bounty-text').text(JSON.parse(bountyObject.data).join(""));
@@ -103,38 +104,44 @@ App = {
     bountyTemplate.find('.bounty-balance').text(bountyObject.balance);
     bountyTemplate.find('.bounty-number').text(index);
     if (bountyObject.bountyOpen) {
-      bountyTemplate.find('.btn-submit').attr('disabled', false);
+      bountyTemplate.find('.btn-sm').attr('data-id', index)
+      if (bountyObject.bountyIssuer != web3.eth.accounts[0]) {
+        bountyTemplate.find('.btn-fulfillment').remove();
+        bountyTemplate.find('.btn-close').remove();
+      }
+    } else {
+      bountyTemplate.find('.btn-group').empty();
+      bountyTemplate.find('.btn-group').append("<div class='btn btn-sm btn-outline-danger'>Bounty Closed</button>")
     }
 
     bountyRow.prepend(bountyTemplate.html());
-
   },
 
   showBounties: function () {
-
+    $('#bountyRow').empty();
 
     var birdBountyInstance;
 
+    App.getNumOfBounties().then(function () {
+      App.contracts.BirdBounty.deployed().then(function (instance) {
+        var promises = [];
 
-    App.contracts.BirdBounty.deployed().then(function (instance) {
-      var promises = [];
-
-      birdBountyInstance = instance;
-      for (i = App.numOfBounties - 1; i >= 0; i--) {
-        promises.push(i);
-        promises.push(birdBountyInstance.getBounty(i))
-      }
-
-      return promises;
-    }).then(function (promises) {
-      Promise.all(promises).then(function (result) {
-        for (i = 0; i < result.length; i += 2) {
-          bountyObject = App.convertToBountyObject(result[i + 1]);
-          App.showBounty(bountyObject, result[i])
+        birdBountyInstance = instance;
+        for (i = 0; i < App.numOfBounties; i++) {
+          promises.push(i);
+          promises.push(birdBountyInstance.getBounty(i))
         }
-      });
-    })
 
+        return promises;
+      }).then(function (promises) {
+        Promise.all(promises).then(function (result) {
+          for (i = 0; i < result.length; i += 2) {
+            bountyObject = App.convertToBountyObject(result[i + 1]);
+            App.showBounty(bountyObject, result[i])
+          }
+        });
+      })
+    });
   },
 
   oraclizeTweet: function () {
@@ -221,7 +228,7 @@ App = {
               console.log(bountyId);
               birdBountyInstance.getBounty(bountyId).then(function (result) {
                 var bountyObject = App.convertToBountyObject(result);
-                App.showBounty(bountyObject, bountyId);
+                App.showBounties();
               });
             });
         });
@@ -249,8 +256,72 @@ App = {
   },
 
   showFulfillBountyInput: function () {
+    $("#fulfill-bountyid-input").empty();
+    $("#fulfill-bountyid-message").empty();
+    $("#fulfill-bird-bounty").prop('disabled', true)
+    $("#fulfill-bird-bounty").removeClass("btn-outline-success").removeClass("btn-outline-danger").addClass("btn-outline-secondary")
+          
+    $("#fulfill-bountyid-input").append("<option selected>Choose...</option>")
+    for (i = App.numOfBounties - 1; i >= 0; i--) {
+      console.log(i)
+      $("#fulfill-bountyid-input").append("<option val='" + i + "'>" + i + "</option>");
+    }
     $("#create-bounty-input").hide();
     $("#fulfill-bounty-input").show();
+  },
+
+  checkBountyFulfill: function () {
+    var birdBountyInstance;
+    var tweetUrl = $('#twitter-url').val();
+    var bountyId = $('#fulfill-bountyid-input').val();
+
+    console.log(tweetUrl, bountyId)
+
+    if (tweetUrl.includes("https://twitter.com/")) {
+      tweetId = tweetUrl.replace("https://twitter.com/", "");
+
+      App.contracts.BirdBounty.deployed().then(function (instance) {
+        birdBountyInstance = instance;
+
+        return birdBountyInstance.fulfillBounty.call(bountyId, tweetId)
+
+      }).then(function (result) {
+        if (result) {
+          $("#fulfill-bountyid-message").text("Looks good! Claim your bounty.")
+          $("#fulfill-bird-bounty").addClass("btn-outline-success").removeClass("btn-outline-danger").removeClass("btn-outline-secondary")
+          $("#fulfill-bird-bounty").prop('disabled', false);
+        }
+      }).catch(function () {
+        $("#fulfill-bountyid-message").text("This tweet won't work...")
+        $("#fulfill-bird-bounty").prop('disabled', true);
+        $("#fulfill-bird-bounty").removeClass("btn-outline-success").addClass("btn-outline-danger").removeClass("btn-outline-secondary")
+      })
+    }
+  },
+
+  claimBounty: function () {
+    var birdBountyInstance;
+    var tweetUrl = $('#twitter-url').val();
+    var bountyId = $('#fulfill-bountyid-input').val();
+
+    console.log(tweetUrl, bountyId)
+
+    if (tweetUrl.includes("https://twitter.com/")) {
+      tweetId = tweetUrl.replace("https://twitter.com/", "");
+
+      App.contracts.BirdBounty.deployed().then(function (instance) {
+        birdBountyInstance = instance;
+
+        return birdBountyInstance.fulfillBounty(bountyId, tweetId);
+      }).then(function () {
+        $("#next-steps").empty();
+        $("#next-steps").append(
+          `<div class="alert alert-success" role="alert">
+            <strong>Nice! You got paid.</strong> Now keep tweeting!
+          </div>`
+        );
+      });
+    }
   }
 
 };
