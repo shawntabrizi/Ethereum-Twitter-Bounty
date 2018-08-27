2e0e604ae32e7645f7af777e9d97b1a0b0b4a67d
Shawn Tabrizi <shawntabrizi@gmail.com>
2018-08-26 19:24:06 -0700

    increase initial oracle contract amount

--- twitter-bounty/migrations/2_deploy_contracts.js
+++ twitter-bounty/migrations/2_deploy_contracts.js
@@ -2,7 +2,7 @@ var TwitterOracle = artifacts.require("TwitterOracle");
 var TwitterBounty = artifacts.require("TwitterBounty");
 
 module.exports = function (deployer) {
-    deployer.deploy(TwitterOracle, { from: web3.eth.accounts[9], gas: 6721975, value: 500000000000000000 })
+    deployer.deploy(TwitterOracle, { from: web3.eth.accounts[9], gas: 6721975, value: 5000000000000000000 })
         .then(function () {
             return deployer.deploy(TwitterBounty, TwitterOracle.address, { gas: 6721975 });
         });
